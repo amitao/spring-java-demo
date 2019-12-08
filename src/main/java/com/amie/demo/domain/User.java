da293db36c9fb87112e4231d594638273ea1ab9d
Amie Thao <amie.c.thao@gmail.com>
2019-12-07 21:46:52 -0600

    add model with user object to get form data

--- src/main/java/com/amie/demo/domain/User.java
+++ src/main/java/com/amie/demo/domain/User.java
@@ -1,5 +1,60 @@
 package com.amie.demo.domain;
 
 public class User {
+	
+	// variables
+	private String fullName;
+	private int age;
+	private boolean employed;
+	private String gender;
+	
+	// empty constructor
+	public User() {
+		
+	}
+	
+	
 
+	public User(String fullName, int age, boolean employed, String gender) {
+		super();
+		this.fullName = fullName;
+		this.age = age;
+		this.employed = employed;
+		this.gender = gender;
+	}
+
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public boolean isEmployed() {
+		return employed;
+	}
+
+	public void setEmployed(boolean employed) {
+		this.employed = employed;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+	
 }
--- src/main/java/com/amie/demo/web/HomeController.java
+++ src/main/java/com/amie/demo/web/HomeController.java
@@ -1,13 +1,26 @@
 package com.amie.demo.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.amie.demo.domain.User;
 
 @Controller
 public class HomeController {
 	
 	@RequestMapping("/")
-	public String home() {
+	public String home(Model model) {
+		// formData key access in User object
+		model.addAttribute("formData", new User());
 		return "index";
 	}
+	
+	@RequestMapping(value = "/create", method = RequestMethod.POST)
+	public String submitForm() {
+		return "";
+	}
+	
+	
 }
