5de360ecf41fca16a5f2632f0be5315555ff866c
Amie Thao <amie.c.thao@gmail.com>
2019-12-07 21:25:40 -0600

    add form to index.html

--- src/main/java/com/amie/demo/web/HomeController.java
+++ src/main/java/com/amie/demo/web/HomeController.java
@@ -1,5 +1,13 @@
 package com.amie.demo.web;
 
-public class HomeController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String home() {
+		return "index";
+	}
 }
