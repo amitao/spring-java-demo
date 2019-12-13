b20821faed16289ca206cdd7d35e295f1f43c92e
Amie Thao <amie.c.thao@gmail.com>
2019-12-12 20:53:45 -0600

    add redirect with data to URL

--- src/main/java/com/amie/demo/web/HomeController.java
+++ src/main/java/com/amie/demo/web/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.amie.demo.domain.User;
 
@@ -22,11 +23,21 @@ public class HomeController {
 	// URL create is where data is submitted to
 	@RequestMapping(value = "/create", method = RequestMethod.POST)
 	// show form input data in html - add object parameters
-	public String submitForm(User user) {
+	public String submitForm(User user, RedirectAttributes attr) {
 		// after form is submitted it redirects to dataResult.html in templates
-		return "dataResult";
+		// return "dataResult";
 		// URL: http://localhost:8080/create
 		// webpage: will display the dataResult.html data
+		
+		// this will redirect to URL display with data
+		attr.addFlashAttribute("user", user);
+		return "redirect:/display";
+	}
+	
+	// Redirect once form is submitted
+	@RequestMapping(value = "/display", method = RequestMethod.GET)
+	public String displayFormData(User user) {
+		return "result";
 	}
 	
 	
